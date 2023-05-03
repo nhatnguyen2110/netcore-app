5d4a860368f7e9729f672fb1c9990f5cb0a85e3c
NhatNguyen <minhnhat2110@gmail.com>
2023-05-03 12:35:07 +0700

    config nlog

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -4,16 +4,24 @@ using Domain;
 using Infrastructure;
 using Infrastructure.Persistence;
 using NLog;
+using NLog.Web;
 using WebAPI.Extensions;
 using WebAPI.Services;
 
 
+// Early init of NLog to allow startup and exception logging, before host is built
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
-LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
-// Add services to the container.
+    // Add services to the container.
 
-builder.Services.AddControllers();
+    builder.Services.AddControllers();
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
@@ -51,6 +59,19 @@ builder.Services.AddControllers();
 
     app.MapControllers();
 
-app.Run();
+    app.Run();
+}
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
+
 
 
